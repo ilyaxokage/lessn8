8c5a67913ec0fb2093ba58e5dbea17d806e15c36
reflexgame123 <Kahn_yr44@student.itstep.org>
2025-09-27 12:51:17 +0300

    ...a

---
+++ main.py
@@ -0,0 +1,20 @@
+# spusok = [666,777,888]
+# i=iter(spusok)
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+
+# spusok=[666,777,888]
+# i=iter(spusok)
+#
+# for j in i:
+#     print(j)
+
+# spusok=[666,777,888]
+# i=iter(spusok)
+#
+# for j in i:
+#     print(j)
+# for j in i:
+#     print(j)
